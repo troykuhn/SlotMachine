a67f13603d5b7dc753fcfb3be846d73768834bd5
Troy Kuhn <troykuhn22@gmail.com>
2020-04-16 14:41:17 -0700

    Basic Slot Machine !

--- SlotMachine/Form1.Designer.cs
+++ SlotMachine/Form1.Designer.cs
@@ -28,13 +28,133 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.pictureBox3 = new System.Windows.Forms.PictureBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.pictureBox4 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+			this.pictureBox1.Location = new System.Drawing.Point(574, 106);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(194, 204);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+			this.pictureBox2.Location = new System.Drawing.Point(301, 106);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(194, 204);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox2.TabIndex = 1;
+			this.pictureBox2.TabStop = false;
+			// 
+			// pictureBox3
+			// 
+			this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+			this.pictureBox3.Location = new System.Drawing.Point(48, 106);
+			this.pictureBox3.Name = "pictureBox3";
+			this.pictureBox3.Size = new System.Drawing.Size(194, 204);
+			this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox3.TabIndex = 2;
+			this.pictureBox3.TabStop = false;
+			this.pictureBox3.UseWaitCursor = true;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(45, 78);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(63, 13);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Credits: 100";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(298, 78);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(35, 13);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Bet: 5";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(546, 78);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(38, 13);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "Win: 0";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(301, 462);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(236, 66);
+			this.button1.TabIndex = 6;
+			this.button1.Text = "SPIN";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// pictureBox4
+			// 
+			this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+			this.pictureBox4.Location = new System.Drawing.Point(0, 0);
+			this.pictureBox4.Name = "pictureBox4";
+			this.pictureBox4.Size = new System.Drawing.Size(807, 456);
+			this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox4.TabIndex = 7;
+			this.pictureBox4.TabStop = false;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size( 800, 450 );
+			this.ClientSize = new System.Drawing.Size(865, 553);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.pictureBox3);
+			this.Controls.Add(this.pictureBox2);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.pictureBox4);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.PictureBox pictureBox3;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.PictureBox pictureBox4;
 	}
 }
 
--- SlotMachine/Form1.cs
+++ SlotMachine/Form1.cs
@@ -16,5 +16,88 @@ namespace SlotMachine
 		{
 			InitializeComponent();
 		}
+
+		//Declaing Each Item
+		public static int p1;
+		public static int p2;
+		public static int p3;
+
+		//Declaring Total, Bet, and Credits
+		public static long credits = 100;
+		public static long total = 0;
+		public static int bet = 5;
+
+		private void Form1_Load( object sender, EventArgs e )
+		{
+			pictureBox1.Image = Image.FromFile( "2.png" );
+			pictureBox2.Image = Image.FromFile( "3.png" );
+			pictureBox3.Image = Image.FromFile( "1.png" );
+			pictureBox4.Image = Image.FromFile( "slots.png" );
+
+			// Transparent background...  
+			pictureBox1.BackColor = Color.Transparent;
+			pictureBox2.BackColor = Color.Transparent;
+			pictureBox3.BackColor = Color.Transparent;
+
+			// Change parent for overlay PictureBox...
+			pictureBox1.Parent = pictureBox4;
+			pictureBox2.Parent = pictureBox4;
+			pictureBox3.Parent = pictureBox4;
+
+		}
+
+		//Generates Random Numbers
+		public static class IntUtil
+		{
+			private static Random random;
+			private static void Init()
+			{
+				if ( random == null ) random = new Random();
+			}
+
+			public static int Random( int min, int max )
+			{
+				Init();
+				return random.Next( min, max );
+			}
+		}
+
+		private void button1_Click( object sender, EventArgs e )
+		{
+			if ( credits >= bet )
+			{
+				credits = credits - bet;
+				label1.Text = "Credits: " + credits.ToString();
+
+				for ( var i = 0; i < 10; i++ )
+				{
+					p1 = IntUtil.Random( 1, 4 );
+					p2 = IntUtil.Random( 1, 4 );
+					p3 = IntUtil.Random( 1, 4 );
+				}
+
+				if ( pictureBox1.Image != null ) pictureBox1.Image.Dispose();
+				pictureBox1.Image = Image.FromFile( p1.ToString() + ".png" );
+
+				if ( pictureBox2.Image != null ) pictureBox2.Image.Dispose();
+				pictureBox2.Image = Image.FromFile( p2.ToString() + ".png" );
+
+				if ( pictureBox1.Image != null ) pictureBox3.Image.Dispose();
+				pictureBox3.Image = Image.FromFile( p3.ToString() + ".png" );
+
+				total = 0;
+
+				if ( p1 == 3 ) total = total + 5;
+				if ( p1 == 2 & p2 == 2) total = total + 10;
+				if ( p1 == 3 & p2 == 3) total = total + 10;
+				if ( p1 == 1 & p2 == 1 & p3 == 1) total = total + 20;
+				if ( p1 == 2 & p2 == 2 & p3 == 2) total = total + 30;
+				if ( p1 == 3 & p2 == 3 & p3 == 3) total = total + 50;
+
+				credits = credits + total;
+				label3.Text = "Win: " + total.ToString();
+				label1.Text = "Credits: " + credits.ToString();
+			}
+		}
 	}
 }
